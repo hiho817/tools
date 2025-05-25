cf86a33f008b3489f858c8131236dce7a64ba58a
hiho817 <kenny0817123@gmail.com>
2025-05-25 09:51:25 +0000

    delete unused test func

--- Data_Processor/static_test.py
+++ Data_Processor/static_test.py
@@ -5,7 +5,8 @@ import os
 import matplotlib.pyplot as plt
 
 # Define the file path
-file_path = 'data/0520/force_test_stand.csv'
+file_path = 'data/0521/force_test_updown.csv'
+# file_path = 'data/0520/force_test_stand.csv'
 
 # Check if file exists
 if not os.path.exists(file_path):
@@ -36,12 +37,13 @@ if missing_columns:
 
 MASS = 20.0  # kg
 GRAVITY = 9.81  # m/s^2
+
 Fz = data['force_Fy_a'] + data['force_Fy_b'] + data['force_Fy_c'] + data['force_Fy_d'] - data['imu_lin_acc_z'] * MASS - GRAVITY * MASS
 Fx = data['force_Fx_a'] + data['force_Fx_b'] + data['force_Fx_c'] + data['force_Fx_d'] - data['imu_lin_acc_x'] * MASS
 
-
 # Define a parameter for the time range to plot
-plot_time = [5.5, 10]  # in seconds, [init, final]
+plot_time = [2, 20]  # in seconds, [init, final]
+# plot_time = [6.5, 20]  # in seconds, [init, final]
 
 # Create time array (1000Hz sampling rate)
 time = np.arange(len(data)) / 1000.0  # time in seconds
@@ -59,7 +61,7 @@ plt.figure(figsize=(10, 6))
 plt.subplot(2, 1, 1)
 plt.plot(time[start_idx:end_idx], np.array(Fz)[start_idx:end_idx], label='Fz')
 plt.xlabel('Time (seconds)')
-plt.ylabel('Force Fz (N)')
+plt.ylabel('Force (N)')
 plt.title('Force Fz')
 plt.legend()
 plt.grid(True)
@@ -67,8 +69,77 @@ plt.grid(True)
 plt.subplot(2, 1, 2)
 plt.plot(time[start_idx:end_idx], np.array(Fx)[start_idx:end_idx], label='Fx')
 plt.xlabel('Time (seconds)')
-plt.ylabel('Force Fx (N)')
+plt.ylabel('Force (N)')
 plt.title('Force Fx')
+plt.legend()
+plt.grid(True)
+
+plt.tight_layout()
+plt.show()
+
+# Plot the sum of forces over the specified time range
+plt.figure(figsize=(10, 6))
+plt.subplot(4, 2, 1)
+plt.plot(time[start_idx:end_idx], np.array(data['force_Fx_a'])[start_idx:end_idx], label='Fx')
+plt.xlabel('Time (seconds)')
+plt.ylabel('Force (N)')
+plt.title('Module A FX (N)')
+plt.legend()
+plt.grid(True)
+
+plt.subplot(4, 2, 2)
+plt.plot(time[start_idx:end_idx], np.array(data['force_Fy_a'])[start_idx:end_idx], label='Fz')
+plt.xlabel('Time (seconds)')
+plt.ylabel('Force (N)')
+plt.title('Module A Fz (N)')
+plt.legend()
+plt.grid(True)
+
+plt.subplot(4, 2, 3)
+plt.plot(time[start_idx:end_idx], np.array(data['force_Fx_b'])[start_idx:end_idx], label='Fx')
+plt.xlabel('Time (seconds)')
+plt.ylabel('Force (N)')
+plt.title('Module B FX (N)')
+plt.legend()
+plt.grid(True)
+
+plt.subplot(4, 2, 4)
+plt.plot(time[start_idx:end_idx], np.array(data['force_Fy_b'])[start_idx:end_idx], label='Fz')
+plt.xlabel('Time (seconds)')
+plt.ylabel('Force (N)')
+plt.title('Module B Fz (N)')
+plt.legend()
+plt.grid(True)
+
+plt.subplot(4, 2, 5)
+plt.plot(time[start_idx:end_idx], np.array(data['force_Fx_c'])[start_idx:end_idx], label='Fx')
+plt.xlabel('Time (seconds)')
+plt.ylabel('Force (N)')
+plt.title('Module C FX (N)')
+plt.legend()
+plt.grid(True)
+
+plt.subplot(4, 2, 6)
+plt.plot(time[start_idx:end_idx], np.array(data['force_Fy_c'])[start_idx:end_idx], label='Fz')
+plt.xlabel('Time (seconds)')
+plt.ylabel('Force (N)')
+plt.title('Module C Fz (N)')
+plt.legend()
+plt.grid(True)
+
+plt.subplot(4, 2, 7)
+plt.plot(time[start_idx:end_idx], np.array(data['force_Fx_d'])[start_idx:end_idx], label='Fx')
+plt.xlabel('Time (seconds)')
+plt.ylabel('Force (N)')
+plt.title('Module D FX (N)')
+plt.legend()
+plt.grid(True)
+
+plt.subplot(4, 2, 8)
+plt.plot(time[start_idx:end_idx], np.array(data['force_Fy_d'])[start_idx:end_idx], label='Fz')
+plt.xlabel('Time (seconds)')
+plt.ylabel('Force (N)')
+plt.title('Module D Fz (N)')
 plt.legend()
 plt.grid(True)
 
